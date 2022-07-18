5135640296988e935936063145a7fcddaff2f3f5
mohamedabdelgwad603 <popmohamed679@yahoo.com>
2022-07-18 11:16:02 +0200

    Edit and add

---
+++ lib/data/data.dart
@@ -0,0 +1,71 @@
+class music {
+  final String title;
+  final String owner;
+  final String urlmusic;
+  final String urlcover;
+
+  music({this.title, this.owner, this.urlmusic, this.urlcover});
+}
+
+List<music> MusicList = [
+  music(
+    title: "أنت لا تفهم الأمر على حقيقته",
+    owner: "ديك الجن",
+    urlmusic: "latfham.mp3",
+    urlcover: "assets/image/3.jpg",
+  ),
+  music(
+    title: "قضايا خاسرة",
+    owner: "ديك الجن",
+    urlmusic: "kdaya.mp3",
+    urlcover: "assets/image/1.jpg",
+  ),
+  music(
+    title: "انزع عنك",
+    owner: "ديك الجن",
+    urlmusic: "enza.mp3",
+    urlcover: "assets/image/2.jpg",
+  ),
+  music(
+    title: "كن ممتناً",
+    owner: "غير معروف",
+    urlmusic: "mmtn.mp3",
+    urlcover: "assets/image/4.jpg",
+  ),
+  music(
+    title: "واجبنا تجاه أطفالنا",
+    owner: "ديك الجن",
+    urlmusic: "atfalna.mp3",
+    urlcover: "assets/image/1.jpg",
+  ),
+  music(
+    title: "من رسائل المعلم فرحان",
+    owner: "ديك الجن",
+    urlmusic: "frhan.mp3",
+    urlcover: "assets/image/1.jpg",
+  ),
+  music(
+    title: "من أوصاف النبي صلى الله عليه و سلم",
+    owner: "الرافعي",
+    urlmusic: "awsaf.mp3",
+    urlcover: "assets/image/2.jpg",
+  ),
+  music(
+    title: "تلاوة من سورة الشورى",
+    owner: "قراّن ",
+    urlmusic: "shwra.mp3",
+    urlcover: "assets/image/3.jpg",
+  ),
+  music(
+    title: "موعد مع نفسي",
+    owner: "ديك الجن",
+    urlmusic: "mwed.mp3",
+    urlcover: "assets/image/6.jpg",
+  ),
+  music(
+    title: "كلمة زيد بن الخطاب",
+    owner: "....",
+    urlmusic: "omar.mp3",
+    urlcover: "assets/image/8.jpg",
+  ),
+];
